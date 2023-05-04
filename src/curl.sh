d235b855ad9b6b3173ab4724f07ac559ca0cfbd7
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2023-05-04 15:54:51 -0700

    Update curl.sh

--- src/curl.sh
+++ src/curl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 buildkite-agent annotate "first error" --style error --context "auto_fix"
-buildkite-agent annotate "second error" --style 'error'
+buildkite-agent annotate --style 'error'
